463c58e334f4f2ef56b6570af71fc4f2392720bf
Michael Lück <michael.lueck@rewe-group.com>
2024-12-09 09:45:12 +0100

    Day 9 P2 not yet solved

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -6,15 +6,24 @@ class Day9(example: String) {
         override fun toString() = id?.toString() ?: "."
     }
 
+    data class Space (val index:Int, val length: Int, val free: Boolean, val fileId : Int?, var firstBlock : Int = -1)
+
     private val blocks : List<Block>
+    private val spaces : List<Space>
 
     init {
-        var idGenerator = 0
-        var newIndex = 0
-        blocks = example.flatMapIndexed { index, number ->
+        var fileIdGenerator = 0
+        spaces = example.mapIndexed { index, number ->
             val isFree = (index % 2) == 1
-            val id: Int? = if(isFree) null else idGenerator++
-            (0..<number.digitToInt()).map { Block(id = id, newIndex++) }
+            val id: Int? = if(isFree) null else fileIdGenerator++
+            Space(index = index, length = number.digitToInt(), free = isFree, fileId = id)
+        }
+        var blockIndex = 0
+        blocks = spaces.flatMap { space ->
+            space.firstBlock = blockIndex
+            (0 ..<space.length).map {
+                Block(id=space.fileId, blockIndex++)
+            }
         }
     }
 
@@ -32,6 +41,42 @@ class Day9(example: String) {
             }
         }
 
-        return blocks.foldIndexed(0) { index, checksum, block -> checksum + (index * (block.id ?: 0))}
+        return checksum()
+    }
+
+    private fun checksum(): Long = blocks.foldIndexed(0) { index, checksum, block -> checksum + (index * (block.id ?: 0)) }
+
+    fun solve2() : Long {
+
+        val fileSpaces = ArrayDeque(spaces.filter { !it.free }.reversed())
+        val freeSpaces = spaces.filter { it.free }.toMutableList()
+
+        while(fileSpaces.isNotEmpty()) {
+            val file = fileSpaces.pop()
+
+            val newSpaceIdx = freeSpaces.indexOfFirst { it.length >= file.length }
+            if(newSpaceIdx == -1) continue
+
+            val newSpace = freeSpaces[newSpaceIdx]
+            (newSpace.firstBlock ..< (newSpace.firstBlock + file.length)).forEach { idx ->
+                this.blocks[idx].id = file.fileId
+            }
+            (file.firstBlock ..< (file.firstBlock + file.length)).forEach { idx ->
+                this.blocks[idx].id = null
+            }
+
+            if(newSpace.length > file.length) {
+                freeSpaces[newSpaceIdx] = Space(
+                    index = newSpace.index,
+                    free = true,
+                    length = newSpace.length - file.length,
+                    fileId = null,
+                    firstBlock = newSpace.firstBlock + file.length)
+            } else {
+                freeSpaces.removeAt(newSpaceIdx)
+            }
+        }
+
+        return checksum()
     }
 }

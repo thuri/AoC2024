933b91ed03a77c3685009bc726037c9984e1240a
Michael Lück <michael.lueck@rewe-group.com>
2024-12-13 07:23:43 +0100

    Day12 P2 not yet solved

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -1,34 +1,6 @@
 class Day12(input: String) {
 
-    class Region (val plots : Set<Plot>) {
-
-        init {
-            regions.add(this)
-        }
-
-        private fun borders() = plots.sumOf { it.borders }
-        private fun area() = plots.size
-
-        fun price() : Long = borders() * area()
-
-        operator fun plus(other : Region) : Region {
-            regions.remove(this)
-            regions.remove(other)
-
-            return Region(this.plots + other.plots).apply{
-
-                this@Region.plots.forEach { it.region = this@apply }
-                other.plots.forEach { it.region = this@apply }
-
-                regions.add(this@apply)
-            }
-        }
-    }
-
-    class Plot(val coord: Point, val plantType: Char, var borders: Long = 0) {
-        var region : Region = Region(setOf(this))
-    }
-
+    private val regions = mutableSetOf<Region>()
     private val plots : List<List<Plot>> =
         input
             .lines()
@@ -38,7 +10,7 @@ class Day12(input: String) {
                 }
             }
 
-    fun solve1(): Long {
+    init  {
         plots.forEachIndexed { y, row ->
             row.forEachIndexed { x, plot ->
                 directions.forEach {direction ->
@@ -46,18 +18,71 @@ class Day12(input: String) {
                     if(plot.plantType == neighbor?.plantType) {
                         plot.region + neighbor.region
                     } else {
-                        plot.borders++
+                        plot.borders.add(direction)
                     }
                 }
             }
         }
+    }
 
+    inner class Region (private val regionPlots : Set<Plot>) {
+
+        init {
+            regions.add(this)
+        }
+
+        private fun borders() = regionPlots.sumOf { it.borders.size.toLong() }
+        private fun area()    = regionPlots.size
+        private fun sides(): Long {
+            val west  = mutableSetOf<Int>()
+            val east  = mutableSetOf<Int>()
+            val north = mutableSetOf<Int>()
+            val south = mutableSetOf<Int>()
+
+            regionPlots.forEach { plot ->
+                   plot.borders.forEach() { border ->
+                       when(border) {
+                           WEST -> west += plot.coord.first
+                           EAST -> east += plot.coord.first
+                           NORTH -> north += plot.coord.second
+                           SOUTH -> south += plot.coord.second
+                       }
+                   }
+            }
+
+            return (west.size + east.size + north.size + south.size).toLong()
+        }
+
+        fun price() : Long = borders() * area()
+        fun price2() = sides() * area()
+
+        operator fun plus(other : Region) : Region {
+            regions.remove(this)
+            regions.remove(other)
+
+            return Region(this.regionPlots + other.regionPlots).apply{
+
+                this@Region.regionPlots.forEach { it.region = this@apply }
+                other.regionPlots.forEach { it.region = this@apply }
+
+                regions.add(this@apply)
+            }
+        }
+    }
+
+    inner class Plot(val coord: Point, val plantType: Char, var borders: MutableSet<Vector> = mutableSetOf()) {
+        var region : Region = Region(setOf(this))
+    }
+
+    fun solve1(): Long {
         return regions.sumOf { it.price() }
     }
 
-    companion object {
+    fun solve2() : Long {
+        return regions.sumOf { it.price2() }
+    }
 
-        private val regions = mutableSetOf<Region>()
+    companion object {
 
         operator fun List<List<Plot>>.get(corrdinates : Point) : Plot? {
             return this.getOrElse(corrdinates.second) { null }?.getOrElse(corrdinates.first) { null }
--- src/test/kotlin/Day12Test.kt
+++ src/test/kotlin/Day12Test.kt
@@ -3,6 +3,16 @@ import org.junit.jupiter.api.Test
 
 class Day12Test {
 
+/*      0123456789
+      0         FF
+      1          F
+      2 VVRRRC FFF
+      3 VVRCCC FFF
+      4 VVVVCJJ F
+
+ */
+
+
     private val example ="""
         RRRRIICCFF
         RRRRIICCCF
@@ -15,6 +25,15 @@ class Day12Test {
         MIIISIJEEE
         MMMISSJEEE
     """.trimIndent()
+//    regionPlots.filter { it.borders.size > 0 }.map { "${it.plantType} ${it.coord} - ${it.borders}" }
+    private val example2 = """
+        AAAAAA
+        AAABBA
+        AAABBA
+        ABBAAA
+        ABBAAA
+        AAAAAA
+    """.trimIndent()
 
     private val input = Day12::class.java.getResource("/Day12.txt")!!.readText()
 
@@ -26,5 +45,15 @@ class Day12Test {
     @Test
     fun puzzle1() {
         assertEquals(1375574, Day12(input).solve1())
+    }
+
+    @Test
+    fun example2() {
+        assertEquals(1206, Day12(example).solve2())
+    }
+
+    @Test
+    fun example2a() {
+        assertEquals(368, Day12(example2).solve2())
     }
 }
